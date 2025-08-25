bf22d885f3099275f9d69a7a8688ba8d04ef9277
XyPolar <xypolar@xyber.inc>
2025-08-25 16:35:39 +0300

    WIP: fixing tests

--- tests/contracts-source/src/MockTrustManagementRouter.sol
+++ tests/contracts-source/src/MockTrustManagementRouter.sol
@@ -22,12 +22,36 @@ contract MockTrustManagementRouter {
         uint256 lockedUntil;
     }
 
+    struct ChunkedX509Cert {
+        bytes bodyPartOne;
+        bytes publicKey;
+        bytes bodyPartTwo;
+        bytes signature;
+        // expiration date
+    }
+
+    struct ChunkedSGXQuote {
+        bytes header;
+        bytes isvReport;
+        bytes isvReportSignature;
+        bytes attestationKey;
+        bytes qeReport;
+        bytes qeReportSignature;
+        bytes qeAuthenticationData;
+    }
 
     /*
      * Below are function from the original TrustManagementRouter contract
      */
 
     function execute(Transaction[] calldata txs, bytes calldata signature, uint256 deadline) external {}
+
+    function initSessionKey(
+        ChunkedX509Cert calldata leaf,
+        ChunkedX509Cert calldata intermediate,
+        ChunkedSGXQuote calldata quote,
+        address sessionKey
+    ) external {}
 
     function deposit(
         address receiver, 
